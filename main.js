36ccc87e57bfb95562f0fc6380fcd48c2b277e5c
Ruben Quizon <quizon@fullsail.edu>
2013-08-15 00:39:46 -0500

    why is it null

---
+++ main.js
@@ -0,0 +1,15 @@
+/* Define each list item variable */
+
+var itemType = document.getElementById("dropList");
+var whatName = document.getElementById("itemName");
+var numberOf = document.getElementById("howMany");
+var handable = document.getElementById("checkYesNo");
+var notes = document.getElementById("textNotes");
+
+/* Test call each id */
+
+console.log (itemType);
+console.log (whatName);
+console.log (numberOf);
+console.log (handable);
+console.log (notes);
