5072b07b4b96f3d177a8fa8afe57ee4123087278
Ruben Quizon <quizon@fullsail.edu>
2013-08-15 20:24:34 -0500

    i hate checkboxes

--- main.js
+++ main.js
@@ -3,20 +3,38 @@
 var itemType = document.getElementById("dropList");
 var whatName = document.getElementById("itemName");
 var numberOf = document.getElementById("howMany");
-var handable = document.getElementById("checkYesNo");
+// var handable = document.getElementsByTagName("checkbox");
 var notes = document.getElementById("textNotes");
+// var areYouSure = document.getElementById("submitter");
 
 /* Test call each id */
 
 console.log (itemType.value);
 console.log (whatName.value);
 console.log (numberOf.value);
-console.log (handable.value);
+// console.log (handable.value);
 console.log (notes.value);
+// console.log (areYouSure.value);
 
-/* Event listeners */
 
-var getValue = function(){
-	console.log(whatName.value);
+/* local storage */
+
+var takeInput = function (){
+	localStorage.setItem("Item Type", itemType.value);
+	localStorage.setItem("Item Name", whatName.value);
+	localStorage.setItem("How Many", numberOf.value);
+//	localStorage.setItem("Carried By Hand", handable.value);
+	localStorage.setItem("Notes", notes.value);
 }
-whatName.addEventListener("blur", getValue);
+
+itemType.addEventListener("blur", takeInput);
+whatName.addEventListener("blur", takeInput);
+numberOf.addEventListener("blur", takeInput);
+// handable.addEventListener("blur", takeInput);
+notes.addEventListener("blur", takeInput);
+
+/* look at local storage */
+
+for (i=0, stori=localStorage.length; i<stori; i++){
+	console.log(localStorage.key(i));
+}
